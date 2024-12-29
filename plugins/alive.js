854a3871669325a2207552fd06497b05e7107fe1
ch4miBOY <ch4mimd@gmail.com>
2024-12-29 16:10:16 -0700

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -10,30 +10,12 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-     }
-let des = `*👋 Hello ${pushname}*
-
-*╔╭────────────╮╕*
-*╭│I'm Alive Now🤹‍♂️  │─◎◎▷*
-*╘╰────────────╯╜*
-*│A CH4MI-MD │Whatsapp Bot Based │Many │Services With A │RealTime* *Automated │Consversational* *││Experience, Enjoy💫.*
-*| So,I Think This Bots Are Useful To You.✨*
-*│*
-*│Type .menu To Get Bot User*
-*│Menu💫*
-*╰───────────────◎◎▷*
- *➣ ʏᴏᴜᴛᴜʙᴇ⚡*= https://youtube.com/@chami-boy-ofc
-
-*➣ ᴡʜᴀᴛꜱᴀᴘᴘ ᴄʜᴀɴɴᴇʟ⚡* = https://whatsapp.com/channel/0029Vazgd9F6WaKffmdKQH2A
-
-*©Cʜ4ᴍɪ-ᴍᴅ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴄʜ4ᴍɪ ʙᴏʏッ*`
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
 }catch(e){
 console.log(e)
 reply(`${e}`)
 }
 })
-
 
 
 //===========menu========
