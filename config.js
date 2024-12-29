e1f4a3c82949b5a243eaf0ae44e68133fa848242
ch4miBOY <ch4mimd@gmail.com>
2024-12-29 13:42:41 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "put sesson id",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/xXZZQrk/a0cbc8b9a3873386.jpg",
+ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi CH4MI-MD Is Online Now 💻\n*💻 Owner* - CH4MI-BOY\n\n*💻 Owner Number* -94703973470",
+SUDO_NB: process.env.SUDO_NB || "94723737086",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public",
+AUTO_VOICE:"true"
+};
+
