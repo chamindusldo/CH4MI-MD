64572438e3b6e9c3269dcd818e635affc15adb1c
ch4miBOY <ch4mimd@gmail.com>
2024-12-29 14:21:50 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put sesson id",
+SESSION_ID: process.env.SESSION_ID || "gAgnVLBY#Fxc8vkyXXI5uxz9y-fDSTvMM68hG85f-0QSr-IbQNc4",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/xXZZQrk/a0cbc8b9a3873386.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi CH4MI-MD Is Online Now 💻\n*💻 Owner* - CH4MI-BOY\n\n*💻 Owner Number* -94703973470",
 SUDO_NB: process.env.SUDO_NB || "94723737086",
