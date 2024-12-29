b774dfbb10a3b44f0f539762f49277cb9c9f1b91
ch4miBOY <ch4mimd@gmail.com>
2024-12-29 12:50:58 -0700

    Create joke.js

---
+++ plugins/joke.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const { cmd } = require('../command');
+
+cmd({
+    pattern: "joke",
+    desc: "😂 Get a random joke",
+    react: "🤣",
+    category: "fun",
+    filename: __filename
+},
+async (conn, mek, m, { from, q, reply }) => {
+    try {
+        const url = 'https://official-joke-api.appspot.com/random_joke';  // API for random jokes
+        const response = await axios.get(url);
+        const joke = response.data;
+
+        const jokeMessage = `
+😂 *Here's a random joke for you!* 😂
+
+*${joke.setup}*
+
+${joke.punchline} 😄
+
+*©Cʜ4ᴍɪ-ᴍᴅ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴄʜ4ᴍɪ ʙᴏʏッ* `;
+
+        return reply(jokeMessage);
+    } catch (e) {
+        console.log(e);
+        return reply("⚠️ ¢συℓ∂η'т ƒєт¢н α נσкє яιgнт ησω. ρℓєαѕє тяу αgαιη ℓαтєя.");
+    }
+});
